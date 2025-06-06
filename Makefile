ad091e5d04fc0f8e185b20dbedec60f3a2a8ef0e
CsarNarciso <cesarpazol1029@gmail.com>
2025-06-06 11:49:22 -0600

    Make makefile cross plattform for windows and linux build/compile options

--- Makefile
+++ Makefile
@@ -1,2 +1,21 @@
+# Command to detect the OS
+UNAME := $(shell uname)
+
+# Compiler and source files
+CXX := g++
+SRC := $(wildcard src/*.cpp)
+OUT := main
+
+
+# Platform-specific flags
+ifeq ($(UNAME), Linux)
+    INCLUDES := $(shell sdl2-config --cflags)
+	LIBS := -lSDL2 -lSDL2_image
+else
+	# Assume Windows (MinGW)
+    INCLUDES := -Iinclude -Iinclude/sdl -Iinclude/headers
+	LIBS := -lmingw32 -lSDL2main -lSDL2 -lSDL2_image
+endif
+
 all:
-	g++ -Iinclude -Iinclude/sdl -Iinclude/headers -Llib -o main src/*.cpp -lmingw32 -lSDL2main -lSDL2 -lSDL2_image
+	$(CXX) $(INCLUDES) -o $(OUT) $(SRC) $(LIBS)
--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,6 @@
 #include <SDL.h>
+#include <iostream>
 
 int main(int argc, char* argv[]) {
-	
-	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-		// Handle initialization error
-		SDL_Log("SDL could not initialize! SDL_Error: %s", SDL_GetError());
-		return 1;
-	}
-
-	SDL_Window* window = SDL_CreateWindow(
-		"My First SDL Window", // Title
-		SDL_WINDOWPOS_UNDEFINED, // X position
-		SDL_WINDOWPOS_UNDEFINED, // Y position
-		640, // Width
-		480, // Height
-		SDL_WINDOW_SHOWN // Flags
-	);
-
-	if (window == nullptr) {
-		// Handle window creation error
-		SDL_Log("Window could not be created! SDL_Error: %s", SDL_GetError());
-		SDL_Quit();
-		return 1;
-	}
+	std::cout << "SDL2 works!" << std::endl;
 }
