7253d56b37dc843cd40019727ea357be36c47958
CsarNarciso <cesarpazol1029@gmail.com>
2025-06-08 19:15:55 -0600

    Detect keyboard events to draw or dissapear image

--- src/main.cpp
+++ src/main.cpp
@@ -9,49 +9,25 @@ SDL_Surface* image2;
 
 //Declare methods (?)
 bool init();
-void kill();
 bool load();
+bool loop();
+void kill();
 
 
-int main(int argc, char* argv[]) {
+int main(int argc, char** args) {
 	
 	//Error on initliazing
-	if(!init()) {
-		
-		//Avoid program to close by itself (until user press a key)
-		system("pause");
-		
-		//Then end program
-		return 1;
-	}
+	if( !init() ) return 1;
 	
 	//Error while loading images (sprites?)
 	if( !load() ) return 1;
 	
-	// Blit image to entire window
-	SDL_BlitSurface( image1, NULL, winSurface, NULL );
-
-	// Blit image to scaled portion of window
-	SDL_Rect dest;
-	dest.x = 160;
-	dest.y = 120;
-	dest.w = 320;
-	dest.h = 240;
-	SDL_BlitScaled( image2, NULL, winSurface, &dest );
+	while( loop() ) {
+		//Wait before processing next frame (kind of thread sleep to avoid overwhelming CPU?)
+		SDL_Delay(10);
+	}
 	
-	//Fill (draw) a rectangle in the window (surface)
-	//SDL_FillRect(winSurface, NULL, SDL_MapRGB(winSurface->format, 255, 255, 255));
-	
-	//Update the window display (kind of re load the now updated surface in the window)
-	SDL_UpdateWindowSurface(window);
-	
-	//Wait (kind of the famous 'game loop' to avoid our game to close automatically)
-	system("pause");
-	
-	//And whenever user press a key (get out manually from game loop):
 	kill();
-	
-	//And end program (successfully, without failures)
 	return 0;
 }
 
@@ -66,7 +42,7 @@ bool init() {
 	}
 	
 	//Create window
-	window = SDL_CreateWindow("title", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1280, 720, SDL_WINDOW_SHOWN);
+	window = SDL_CreateWindow("title", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_SHOWN);
 	
 	//If creating windows failed
 	if(!window) {
@@ -108,6 +84,8 @@ bool load() {
 	//Format surfaces (?)
 	//Bmp files (kind of image, sprite) is loaded into a temporary surface, this temp surface is formated/converted as 
 	//part of the main win surface (a sprite in to it), and the result is allocated into the main image surface
+	//but the images/sprites/surfaces are still not loaded into the main surface, they were just "created", "loaded" by the
+	//bytes of the bmp files, to actually place them in the main surface we need to "Blit" them in to
 	image1 = SDL_ConvertSurface(temp1, winSurface->format, 0);
 	image2 = SDL_ConvertSurface(temp2, winSurface->format, 0);
 	
@@ -125,6 +103,53 @@ bool load() {
 	return true;
 }
 
+
+bool loop() {
+	
+	SDL_Event e;
+	
+	// Blit image 1 to entire window
+	SDL_BlitSurface( image1, NULL, winSurface, NULL );
+	
+	//Process events (queued ones): if there is at least one
+	
+	static bool renderImage2; //static to persist value in this function and not reset to null
+	//because it resets to null on every frame and in theory, it will become true after the conditions below if we
+	//hold a key, for example, however, SDL does not queue events (holding a key will throw as many events as we persist)
+	//as fast as the frame rate of our computer/program/code, this means, some events are not queued, that means
+	//sometimes it does not enter the loop (due to no queued events) and become true even if we are holding a key	
+	while(SDL_PollEvent(&e) != 0) {
+		
+		switch(e.type) {
+			
+			case SDL_QUIT:
+					return false;
+			case SDL_KEYDOWN:
+					renderImage2 = true;
+					break;
+			case SDL_KEYUP:
+					renderImage2 = false;
+					break;
+		}
+	}
+	
+	if(renderImage2) {
+		
+		SDL_Rect dest; //dimentions?
+		dest.x = 160;
+		dest.y = 120;
+		dest.w = 320;
+		dest.h = 240;
+		
+		//Blit image 2 scaled (using prev dimentions and positions)
+		SDL_BlitScaled(image2, NULL, winSurface, &dest);
+	}
+	
+	//Refresh window
+	SDL_UpdateWindowSurface(window);
+	
+	return true; //and continue game loop
+}
 
 
 void kill() {
