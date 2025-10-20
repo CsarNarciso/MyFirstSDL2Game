eda7b30355ed3fe5a23b5ac32d9e53d0e70b0ea5
CsarNarciso <cesarpazol1029@gmail.com>
2025-10-20 16:46:30 -0600

    feat: separate player and map logic from main file. This commit moves entities collision and movement logic to their own classes. It moves map collision check logic to Map class.

---
+++ include/Direction.hpp
@@ -0,0 +1,3 @@
+#pragma once
+
+enum class Direction { UP, LEFT, DOWN, RIGHT };
--- src/entity/entity.cpp
+++ src/entity/entity.cpp
@@ -1,7 +1,8 @@
 #include <SDL2/SDL.h>
 
-#include "../include/Entity.hpp"
-#include "../include/Math.hpp"
+#include "../../include/Math.hpp"
+
+#include "../../include/entity/Entity.hpp"
 
 Entity::Entity(Vector2f p_pos, SDL_Texture* p_tex)
 	:pos(p_pos), tex(p_tex)
@@ -12,32 +13,20 @@ Entity::Entity(Vector2f p_pos, SDL_Texture* p_tex)
 	currentFrame.h = 32; 
 }
 
-Vector2f& Entity::getPos()
-{
-	return pos;
-}
-
-void Entity::setPos(int x, int y)
-{
-	pos = Vector2f(x, y);
-}
-
 SDL_Texture* Entity::getTex()
-{
-	return tex;
-}
+{return tex;}
 
 SDL_Rect Entity::getCurrentFrame()
-{
-	return currentFrame;
-}
+{return currentFrame;}
+
+void Entity::setPos(int x, int y)
+{pos = Vector2f(x, y);}
+
+Vector2f& Entity::getPos()
+{return pos;}
 
 void Entity::setAngle(float p_angle)
-{
-	angle = p_angle;
-}
+{angle = p_angle;}
 
 float Entity::getAngle()
-{
-	return angle;
-}
+{return angle;}
